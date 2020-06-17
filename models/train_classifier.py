a16813574e16a3f5807f1985fa65b6ad8a66f981
Leandro Guardia <lean.guardia93@gmail.com>
2020-06-18 00:40:05 +0100

    Feature: Implement simple ML pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,83 @@
 import sys
 
+from sqlalchemy import create_engine
+import pandas as pd
+
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+nltk.download(['punkt', 'wordnet'])
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors   import KNeighborsClassifier
+from sklearn.ensemble    import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
+
+import pickle
+
 
 def load_data(database_filepath):
-    pass
+    """Loads messages from database and creates a DataFrame"""
+
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table("messages", engine)
+    # features = ['message'], 'original', 'genre']
+    category_names  = ['related', 'request', 'offer', 'aid_related', 'medical_help',
+                        'medical_products', 'search_and_rescue', 'security', 'military',
+                        'child_alone', 'water', 'food', 'shelter',  'clothing', 'money',
+                        'missing_people', 'refugees', 'death', 'other_aid',
+                        'infrastructure_related', 'transport', 'buildings', 'electricity',
+                        'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure',
+                        'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
+                        'other_weather', 'direct_report']
+    X = df.message.values
+    y = df[category_names].values
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    """Performs NLP transformations"""
+
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower()) # normalization
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    return Pipeline([
+        ('tokenize', CountVectorizer(tokenizer=tokenize)),
+        ('tdf',      TfidfTransformer()),
+        ('cls',      MultiOutputClassifier(RandomForestClassifier()))
+    ])
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+def evaluate_model(model, X_test, y_test, category_names):
+    """Calculates Precision, Recall and F1_score for all targets individually"""
+
+    y_pred = model.predict(X_test)
+    for index, y_test_col in enumerate(y_test.transpose()):
+        print(category_names[index])
+        # print(y_test_col, y_pred.T[index])
+        print(classification_report(y_test_col, y_pred.T[index]))
 
 
 def save_model(model, model_filepath):
-    pass
+    """Stores the model in a pickle file"""
+
+    pkl_filename = model_filepath
+    with open(pkl_filename, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
